ac0eb46876d67288cd41fdddae25469a59c49ff6
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-06-21 14:59:35 +0900

    todo アプリ

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator; 
+use Illuminate\Support\Facades\Auth;      
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::orderBy('created_at', 'asc')->get();
+    return view('books', [
+        'books' => $books
+    ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+                //バリデーション
+            $validator = Validator::make($request->all(), [
+                'item_name' => 'required|min:3|max:255',
+                'published'   => 'required',
+        ]);
+
+        //バリデーション:エラー 
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        //以下に登録処理を記述（Eloquentモデル）
+
+        // Eloquentモデル
+        $books = new Book;
+        $books->item_name   = $request->item_name;
+        $books->published   = $request->published;
+        $books->save(); 
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        return view('booksedit', ['book' => $book]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'published' => 'required',
+       ]);
+       //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect('/booksedit/'.$request->id)
+                ->withInput()
+                ->withErrors($validator);
+       }
+       
+       //データ更新
+       $books = Book::find($request->id);
+       $books->item_name   = $request->item_name;
+       $books->published   = $request->published;
+       $books->save();
+       return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+   //** ↓ 下をコピー ↓ **    
+    
+    $book->delete();       //追加
+    return redirect('/');  //追加
+    
+     //** ↑ 上をコピー ↑ **
+    }
+}
